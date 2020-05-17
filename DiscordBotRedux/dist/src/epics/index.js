94ba4e2ac3f68b43d5204e2ba2809fca7f475b9a
grantB <grantmbiggert@gmail.com>
2020-05-17 00:04:54 -0400

    remove dist folder

--- DiscordBotRedux/dist/src/epics/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const default_1 = require("./default");
-exports.rootEpics = [...default_1.defaultEpics];
-//# sourceMappingURL=index.js.map
