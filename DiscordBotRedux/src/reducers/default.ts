3d3c70f9406788574741058f5cfda8515f5b20a0
grantB <grantmbiggert@gmail.com>
2020-05-19 22:06:02 -0400

    reducer improvement

--- DiscordBotRedux/src/reducers/default.ts
+++ DiscordBotRedux/src/reducers/default.ts
@@ -1,13 +1,13 @@
 import { MessageEventData } from './../models/message-event-data';
 import { Maybe } from '../monads';
 import { DefaultActions } from '../actions';
-export function sayReducer(state, action: DefaultActions.Say): Maybe<MessageEventData> {
+export function sayReducer(state = Maybe.of(null), action: DefaultActions.Say): Maybe<MessageEventData> {
     debugger;
     switch (action.type) {
         case DefaultActions.ActionTypes.SAY:
             return Maybe.of(action.payload)
         default:
-            return state ? state : Maybe.nothing();
+            return state
     }
 }
 
